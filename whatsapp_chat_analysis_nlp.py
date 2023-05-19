a84a11910f0b4d03a1d319496d3f10b4d3a55be9
Aditya Jaiswal <97625508+ysAditya@users.noreply.github.com>
2023-05-19 18:36:42 +0530

    Update whatsapp_chat_analysis_nlp.py

--- whatsapp_chat_analysis_nlp.py
+++ whatsapp_chat_analysis_nlp.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/16pYwVRrwHyYpOtKMKOxITE7VkLxqhcJf
 """
 
+pip install wordcloud
 from urlextract import URLExtract
 from wordcloud import WordCloud
 import pandas as pd
